3bbe50711d5d9e2a87a2893b644e4de23b1db98a
Kok Foong Lee <kfl31@cam.ac.uk>
2025-07-18 16:22:28 +0800

    kok-foong/1/exposure-calculation-agent: modified generate_results to generate 1 file per exposure dataset

--- agent/interactor/generate_results.py
+++ agent/interactor/generate_results.py
@@ -6,10 +6,11 @@ import agent.utils.constants as constants
 from pathlib import Path
 from rdflib.plugins.sparql.parser import parseQuery
 from agent.utils.stack_configs import BLAZEGRAPH_URL, ONTOP_URL
-from agent.objects.calculation_metadata import CalculationMetadata
 import csv
 import io
 from shapely import wkt
+from collections import defaultdict
+import json
 
 logger = agentlogging.get_logger('dev')
 
@@ -20,25 +21,18 @@ generate_results_bp = Blueprint(
 @generate_results_bp.route('/', methods=['GET'])
 def non_trajectory():
     from agent.utils.kg_client import kg_client
-    # rdf type of calculation type
-    rdf_type = request.args.get('rdf_type')
-
-    # metadata for calculation instance
-    distance = request.args.get('distance')
-
     # IRI(s) of subject to calculate
     subject = request.args.get('subject')
     exposure_table = request.args.get('exposure_table')
-
-    # for trajectory time series
-    upperbound = request.args.get('upperbound')
-    lowerbound = request.args.get('lowerbound')
 
     # query to obtain subject IRIs
     subject_query_file = request.args.get('subject_query_file')
 
     # query for user facing label of subject IRI, e.g. postcode value
     subject_label_query_file = request.args.get('subject_label_query_file')
+
+    # rdf type of calculation type
+    rdf_type = request.args.get('rdf_type')
 
     if subject is not None and subject_query_file is not None:
         raise Exception('Provide subject or subject_query_file, but not both')
@@ -50,22 +44,10 @@ def non_trajectory():
     # get dataset iri to pass the core calculation agent
     exposure_dataset_iri = get_dataset_iri(table_name=exposure_table)
 
-    calculate_metadata = CalculationMetadata(
-        rdf_type=rdf_type, distance=distance, upperbound=upperbound, lowerbound=lowerbound)
-
-    var = 'var'
-    query_result = kg_client.remote_store_client.executeQuery(
-        calculate_metadata.get_query(var))
-    if query_result.length() != 1:
-        raise Exception(
-            'Queried size for calculation instances: ' + str(query_result.length()))
-
-    calculation_iri = query_result.getJSONObject(0).getString(var)
-
     # returns IRI to result
     logger.info('Querying results')
     subject_to_result_dict = _get_subject_to_result_dict(
-        subject=subject, exposure=exposure_dataset_iri, calculation_iri=calculation_iri)
+        subject=subject, exposure=exposure_dataset_iri, calculation_type=rdf_type)
 
     # returns IRI to label
     logger.info('Querying label')
@@ -86,33 +68,60 @@ def non_trajectory():
 
 @generate_results_bp.route('/trajectory', methods=['GET'])
 def trajectory():
+    from agent.utils.kg_client import kg_client
+    # rdf type of calculation type
+    rdf_type = request.args.get('rdf_type')
+
+    # metadata for calculation instance
+    distance = request.args.get('distance')
+
+    # IRI(s) of subject to calculate
+    subject = request.args.get('subject')
+    exposure_table = request.args.get('exposure_table')
+
+    # for trajectory time series
+    upperbound = request.args.get('upperbound')
+    lowerbound = request.args.get('lowerbound')
+
+    # not general, assumes epoch seconds
+    if upperbound is not None:
+        upperbound = int(upperbound)
+
+    if lowerbound is not None:
+        lowerbound = int(lowerbound)
+
     return ''
 
 
-def _get_subject_to_result_dict(subject, exposure, calculation_iri):
+def _get_subject_to_result_dict(subject, exposure, calculation_type):
     from agent.utils.kg_client import kg_client
-    subject_to_result_dict = {}
+    subject_to_result_dict = defaultdict(lambda: defaultdict(dict))
 
     for chunk in _chunk_list(subject):
         values = " ".join(f"<{s}>" for s in chunk)
         query = f"""
-        SELECT ?subject ?value
+        SELECT ?subject ?value ?distance
         WHERE {{
-            VALUES ?subject {{{values}}}
-            ?derivation <{constants.IS_DERIVED_FROM}> ?subject;
-                <{constants.IS_DERIVED_FROM}> <{exposure}>;
-                <{constants.IS_DERIVED_USING}> <{calculation_iri}>.
-            ?result <{constants.BELONGS_TO}> ?derivation;
-                <{constants.HAS_VALUE}> ?value
+            ?calculation a <{calculation_type}>;
+                <{constants.HAS_DISTANCE}> ?distance.
+            SERVICE <{ONTOP_URL}> {{VALUES ?subject {{{values}}}
+                ?derivation <{constants.IS_DERIVED_FROM}> ?subject;
+                    <{constants.IS_DERIVED_FROM}> <{exposure}>;
+                    <{constants.IS_DERIVED_USING}> ?calculation.
+                ?result <{constants.BELONGS_TO}> ?derivation;
+                    <{constants.HAS_VALUE}> ?value
+            }}
         }}
         """
 
-        query_result = kg_client.ontop_client.executeQuery(query)
+        # remote store client gives a Java JSONArray
+        query_result = json.loads(
+            kg_client.remote_store_client.executeQuery(query).toString())
 
-        for i in range(query_result.length()):
-            iri = query_result.getJSONObject(i).getString('subject')
-            result = query_result.getJSONObject(i).getDouble('value')
-            subject_to_result_dict[iri] = result
+        for item in query_result:
+            iri = item['subject']
+            distance = item['distance']
+            subject_to_result_dict[iri][distance] = item['value']
 
     return subject_to_result_dict
 
@@ -136,8 +145,7 @@ def _get_subjects(subject_query_file):
 
     logger.info(
         'Querying subject IRIs with provided SPARQL query template')
-    query_result = kg_client.remote_store_client.executeFederatedQuery(
-        [ONTOP_URL, BLAZEGRAPH_URL], subject_query)
+    query_result = kg_client.ontop_client.executeQuery(subject_query)
 
     logger.info('Received ' + str(query_result.length()) + ' IRIs')
 
@@ -191,11 +199,14 @@ def _create_csv(subject_to_result_dict, subject_to_label_dict, subject_to_point_
         lat = subject_to_point_dict[subject].y
         lng = subject_to_point_dict[subject].x
 
-        data.append({'label': label, 'value': value, 'lat': lat, 'lng': lng})
+        data.append({'postal_code': label, 'lat': lat, 'lng': lng} | value)
 
     output = io.StringIO()
-    writer = csv.DictWriter(
-        output, fieldnames=['label', 'value', 'lat', 'lng'])
+    if data:
+        writer = csv.DictWriter(output, fieldnames=data[0].keys())
+    else:
+        writer = csv.DictWriter(
+            output, fieldnames=['postal_code', 'lat', 'lng'])
     writer.writeheader()
     writer.writerows(data)
 
@@ -229,8 +240,7 @@ def _get_subject_to_point_dict(subject):
         query_list.append(query)
 
     for query in query_list:
-        query_result = kg_client.remote_store_client.executeFederatedQuery(
-            [BLAZEGRAPH_URL, ONTOP_URL], query)
+        query_result = kg_client.ontop_client.executeQuery(query)
 
         for i in range(query_result.length()):
             sub = query_result.getJSONObject(i).getString('subject')
